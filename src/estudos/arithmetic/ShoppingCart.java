a7e2e90208ae5ceebcd9d7b5eb423b7ca13b055a
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-03 13:30:56 -0300

    feat: finished shopping cart exercise

---
+++ src/estudos/arithmetic/ShoppingCart.java
@@ -0,0 +1,33 @@
+package estudos.arithmetic;
+
+import java.util.Scanner;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+
+        // SHOPPING CART PROGRAM
+
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        int quantity;
+        double price;
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = quantity * price;
+
+        System.out.println("You have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is: " + total);
+
+        scanner.close();
+    }
+}
