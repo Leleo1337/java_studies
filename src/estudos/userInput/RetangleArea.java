74be1106ed1652a5995424459807abb34b7abd6f
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-03 11:44:47 -0300

    feat: finished userInput section

---
+++ src/estudos/userInput/RetangleArea.java
@@ -0,0 +1,28 @@
+package estudos.userInput;
+
+import java.util.Scanner;
+
+public class RetangleArea {
+    public static void main(String[] args) {
+        
+        // calculate area of a retangle
+
+        double width = 0;
+        double heigth = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter a heigth: ");
+        heigth = scanner.nextDouble();
+
+        area = width * heigth;
+
+        System.out.println("A area desse retangulo é: " + area);
+
+        scanner.close();
+    }
+}
--- src/estudos/userInput/userInput.java
+++ src/estudos/userInput/userInput.java
@@ -1,7 +1,28 @@
 package estudos.userInput;
 
+import java.util.Scanner;
+
 public class UserInput {
-    public static void main(String[] args){
-        System.out.println("oi");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("What is your gpa: ");
+        double gpa = scanner.nextDouble();
+
+        System.out.print("Are you a student ? (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+
+        System.out.println("Hello " + name);
+        System.out.println("Your are " + age + " years old");
+        System.out.println("Your gpa is " + gpa);
+        System.out.println(isStudent ? "You are a student" : "You are not a student");
+
+        scanner.close();
     }
 }
