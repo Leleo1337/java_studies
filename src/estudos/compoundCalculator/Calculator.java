2925aece864dea045f5cef090fdbcda3603d3434
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-04 13:53:35 -0300

    feat: finished compound calculator

---
+++ src/estudos/compoundCalculator/Calculator.java
@@ -0,0 +1,36 @@
+package estudos.compoundCalculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        // compount interest calculator
+
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int timesComunded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amout: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the # of times compounded per year: ");
+        timesComunded = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesComunded, timesComunded * years);
+
+        System.out.printf("The amount after %d year/s is $%.2f", years, amount);
+
+        scanner.close();
+    }
+}
