70b7b9a554936aff0b92d52c48baad0ab7b4db30
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-11 17:06:51 -0300

    feat: enchanced challenge with ranges

--- src/estudos/controlflow/NumberGuessingGame.java
+++ src/estudos/controlflow/NumberGuessingGame.java
@@ -10,18 +10,29 @@ public class NumberGuessingGame {
         Scanner scanner = new Scanner(System.in);
         int guess = -1;
         int attempts = 1;
-        double randomNumber = Math.ceil(Math.random() * 10);
+        int[] range = {0, 10};
 
         System.out.println("Number guessing game!");
-        System.out.println("Guess a number between 1-10!");
-        while (guess != randomNumber) {
-            System.out.print("Your guess: ");
-            guess = scanner.nextInt();
+        System.out.println("Enter the range of the randoms");
+        System.out.print("Enter range 1: ");
+        range[0] = scanner.nextInt();
+        System.out.print("Enter range 2: ");
+        range[1] = scanner.nextInt();
 
+        System.out.printf("Guess a number between %d - %d: ", range[0], range[1]);
+
+        double randomNumber = Math.ceil(Math.random() * (range[1] - range[0]) + range[0]);
+
+        while (guess != randomNumber) {
+            guess = scanner.nextInt();
+            if (guess > randomNumber) {
+                System.out.print("TOO HIGH! try again: ");
+            } else if (guess < randomNumber) {
+                System.out.print("TOO LOW! try again: ");
+            }
             if (guess == randomNumber) {
-                System.out.printf("Congrats! You guessed the random number %.0f in %d attempts! \n", randomNumber, attempts);
+                    System.out.printf("Congrats! You guessed the random number %.0f in %d attempts! \n", randomNumber, attempts);
             } else {
-                System.out.println("Wrong number");
                 attempts++;
             }
         }
