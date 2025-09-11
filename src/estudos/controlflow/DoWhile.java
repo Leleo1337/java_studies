94ce94b1f3f00f8e2d54e8476f576ba666d09e02
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-11 16:51:37 -0300

    feat: finish do while

---
+++ src/estudos/controlflow/DoWhile.java
@@ -0,0 +1,21 @@
+package estudos.controlflow;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 0;
+
+        do {
+            System.out.print("Enter a number between 1-10: ");
+            number = scanner.nextInt();
+        } while (number < 1 || number > 10);
+
+        System.out.println("You picked: " + number);
+
+        scanner.close();
+    }
+}
