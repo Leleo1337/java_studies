137ccb5de3917143d29b8fca46d61e4e1d4a6eb3
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-17 17:53:55 -0300

    feat: finish constructors

---
+++ src/estudos/OOP/constructors/Main.java
@@ -0,0 +1,36 @@
+package estudos.OOP.constructors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // constructor = A special method to initialzie objects
+        //               You can pass arguments to a constructor
+        //               and set up initial values
+
+        Student student1 = new Student("Spongebob", 30, 3.2);
+        Student student2 = new Student("Leo", 17, 10);
+        Student student3 = new Student("Sandy", 27, 4.0);
+
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
+
+        student1.study();
+
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+
+        student2.study();
+
+        System.out.println(student3.name);
+        System.out.println(student3.age);
+        System.out.println(student3.gpa);
+        System.out.println(student3.isEnrolled);
+
+        student3.study();
+
+    }
+}
