f68ee197c83e6ddf1ce92946e9d87dc0c26535c4
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-23 17:48:22 -0300

    feat: add price < 0 validation

--- src/estudos/OOP/getters_setters/Car.java
+++ src/estudos/OOP/getters_setters/Car.java
@@ -23,12 +23,16 @@ public class Car {
     String getPrice() {
         return "$" + this.price;
     }
-    
+
     void setColor(String color) {
         this.color = color;
     }
 
     void setPrice(int price) {
-        this.price = price;
+        if (price < 0) {
+            System.out.println("Price can't be less than zero");
+        } else {
+            this.price = price;
+        }
     }
 }
