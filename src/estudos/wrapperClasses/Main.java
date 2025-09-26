ebda5382fa5b450e6a66a00de10c661d6f238c97
leleo1337 <leandroroncatto18@gmail.com>
2025-09-26 16:02:17 -0300

    feat: finish wrapper classes

---
+++ src/estudos/wrapperClasses/Main.java
@@ -0,0 +1,27 @@
+package estudos.OOP.wrapperClasses;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Wrapper classes = Allow primitive values (int, char, double, boolean)
+        //                   to be used as objects. "Wrap them in an object"
+        //                   Generally, don't wrap primitives unless you need an object.
+        //                   Allows use of Collections Framework and static Utility Methods.
+
+        // Autoboxing
+        Integer a = 123;
+        Double b = 3.14;
+        Character c = '$';
+        Boolean d = true;
+
+        // Unboxing
+        int e = a;
+        double f = b;
+        char g = c;
+        boolean h = d;
+
+        System.out.println(a.toString() + b);
+        System.out.println();
+
+    }
+}
