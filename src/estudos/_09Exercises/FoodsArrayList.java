52fa1fd568b97fd314b9a28cae961737b696ee56
Leandro <leandroroncatto18@gmail.com>
2025-09-26 16:51:19 -0300

    feat: finish array list exercise

---
+++ src/estudos/_09Exercises/FoodsArrayList.java
@@ -0,0 +1,29 @@
+package estudos._09Exercises;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FoodsArrayList {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> foods = new ArrayList<>();
+        int numOfFoods;
+
+        System.out.print("Enter the # of food you would like: ");
+        numOfFoods = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numOfFoods; i++) {
+            System.out.printf("Enter food %d: ", i + 1);
+
+            String food = scanner.nextLine();
+            foods.add(food);
+        }
+
+        System.out.println(foods);
+
+        scanner.close();
+    }
+}
