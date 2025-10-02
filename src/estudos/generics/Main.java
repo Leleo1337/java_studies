fd287d9e03fb6acaedd4dd572060129f9d6b0478
Leandro <leandroroncatto18@gmail.com>
2025-10-02 17:09:44 -0300

    feat: finish generics

--- src/estudos/generics/Main.java
+++ src/estudos/generics/Main.java
@@ -11,10 +11,24 @@ public class Main {
         //            <String> type argument (specifies the type)
 
         ArrayList<String> fruits = new ArrayList<>();
+        Box<String> box = new Box<>();
+        Product<String, Double> product1 = new Product<>("banana", 0.50);
+        Product<String, Integer> product2 = new Product<>("ticket", 15);
 
         fruits.add("apple");
-        fruits.add("orange");
-        fruits.add("banana");
+        System.out.println(fruits);
+
+        box.setItem("banana");
+        System.out.println(box.getItem());
+
+        System.out.println(product1.getItem());
+        System.out.println(product1.getPrice());
+        System.out.printf("The product is: %s | The price is: %.2f$\n", product1.item, product1.price);
+
+        System.out.println(product2.getItem());
+        System.out.println(product2.getPrice());
+        System.out.printf("The product is: %s | The price is: %d$\n", product2.item, product2.price);
+
 
     }
 
